aab5410c7b29e64e8ce9c4af32a525a2ea3e5b25
Mukesh Patel <mukesh_p004@yahoo.com>
2020-05-11 23:56:54 +0530

    resolved home page slider issue

---
+++ system/config/isenselabs/whatsappshare.php
@@ -0,0 +1,14 @@
+<?php
+
+$_['whatsappshare_moduleName'] = 'whatsappshare';
+$_['whatsappshare_moduleVersion'] = '3.3.4';
+
+
+$_['whatsappshare_modulePath'] = 'extension/module/whatsappshare';
+$_['whatsappshare_callModel'] = 'model_extension_module_whatsappshare';
+
+$_['whatsappshare_extensionLink'] = 'marketplace/extension';
+$_['whatsappshare_extensionLink_type'] = '&type=module';
+
+$_['whatsappshare_status_group']       = 'module_whatsappshare';
+$_['whatsappshare_status_value']       = 'module_whatsappshare_status';
